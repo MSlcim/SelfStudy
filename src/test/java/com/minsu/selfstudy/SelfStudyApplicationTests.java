3f4aea877f4d704995b275852c66a092cc6ae829
minsukim <kimminsu@gmail.com>
2022-04-29 14:43:58 +0900

    Initial commit

---
+++ src/test/java/com/minsu/selfstudy/SelfStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minsu.selfstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
